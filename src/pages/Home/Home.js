3b55cb90c066bbd0b3cfea9e6d4321c24abd437c
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2022-04-19 12:45:12 -0400

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,7 +3,10 @@ import WeeklySchedule from '../../components/WeeklySchedule/WeeklySchedule'
 
 function Home() {
   return (
-    <WeeklySchedule toolbar={false}/>
+    <>
+      <img src='https://raw.githubusercontent.com/dreamsbydutch/gshl/main/public/assets/Logos/League/GSHLCupLogo.png' alt='GSHL Cup logo' />
+      <WeeklySchedule toolbar={false}/>
+    </>
   )
 }
 
