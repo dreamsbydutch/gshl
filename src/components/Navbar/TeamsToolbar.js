60b77de2aada0bc927a54f27fe92b3760903c49e
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2022-07-25 13:51:09 -0400

    Update TeamsToolbar.js

--- src/components/Navbar/TeamsToolbar.js
+++ src/components/Navbar/TeamsToolbar.js
@@ -4,9 +4,9 @@ import { useTeams } from '../../hooks/getStandings'
 import LoadingSpinner from '../../utils/LoadingSpinner/LoadingSpinner'
 
 function TeamsToolbar(props) {
-  const teamsData = useTeams('2021-22')
+  const teamsData = useTeams('2022')
   if (teamsData.isLoading) { return <LoadingSpinner /> }
-  const data = [teamsData.data.data.filter(obj => obj.Conference === 'HH' && obj['2021-22']), teamsData.data.data.filter(obj => obj.Conference === 'SV' && obj['2021-22'])]
+  const data = [teamsData.data.data.filter(obj => obj.Conference === 'HH' && obj['2022']), teamsData.data.data.filter(obj => obj.Conference === 'SV' && obj['2022'])]
   return (
     <div className='team-toolbars'>
       {data.map(group => (
@@ -14,7 +14,7 @@ function TeamsToolbar(props) {
           {group.map((item, i) => (
             <>
               {i !== 0 && <span className='nav-border-line' />}
-              <div key={item['2021-22']} onClick={() => props.setter(item['2021-22'])} className={props.active === item['2021-22'] ? 'active team-toolbar-logo' : 'team-toolbar-logo'} >
+              <div key={item['2022']} onClick={() => props.setter(item['2022'])} className={props.active === item['2022'] ? 'active team-toolbar-logo' : 'team-toolbar-logo'} >
                 <img src={item.LogoURL} alt='Team logo' />
               </div>
             </>
