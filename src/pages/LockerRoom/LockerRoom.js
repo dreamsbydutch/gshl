4d8f266ddd0f2de1380e081698ecde1acf8e475c
Curtis Hough <chough14@gmail.com>
2022-09-08 02:49:54 -0400

    new site

--- src/pages/LockerRoom/LockerRoom.js
+++ src/pages/LockerRoom/LockerRoom.js
@@ -2,8 +2,8 @@ import './LockerRoom.css'
 import React, { useState } from 'react'
 import TeamsToolbar from '../../components/Navbar/TeamsToolbar'
 import { currentSeason } from '../../utils/constants'
-import TeamPlayerContracts from './components/TeamPlayerContracts/TeamPlayerContracts'
-import TeamPlayerStats from './components/TeamPlayerStats/TeamPlayerStats'
+// import TeamPlayerContracts from './components/TeamPlayerContracts/TeamPlayerContracts'
+// import TeamPlayerStats from './components/TeamPlayerStats/TeamPlayerStats'
 
 function LockerRoom() {
   const [teamID, setTeamID] = useState(null)
