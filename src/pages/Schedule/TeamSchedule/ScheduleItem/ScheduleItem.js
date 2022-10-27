18033f82a157ea933c9c9b3927c102cedefeba22
Curtis Hough <chough14@gmail.com>
2022-10-27 17:39:11 -0400

    fix team schedule score undefined issue

--- src/pages/Schedule/TeamSchedule/ScheduleItem/ScheduleItem.js
+++ src/pages/Schedule/TeamSchedule/ScheduleItem/ScheduleItem.js
@@ -5,19 +5,21 @@ import './ScheduleItem.css'
 function ScheduleItem(props) {
   var opponent = props.data.AwayTeamData.id === props.teamID ? props.data.HomeTeamData : props.data.AwayTeamData
   return (
-    <Link to={"/matchup/"+props.data.id}>
-    <div className={props.data.GameType === "CC" || props.data.GameType === "NC" || props.data.GameType === "RS" ? 'team-schedule-item ' + opponent.Conference : 'team-schedule-item ' + props.data.GameType}>
-      <div className={'week'}>
-        {props.data.GameType === "CC" || props.data.GameType === "NC" || props.data.GameType === "RS" ? props.data.WeekNum : props.data.GameType}
+    <Link to={"/matchup/" + props.data.id}>
+      <div className={props.data.GameType === "CC" || props.data.GameType === "NC" || props.data.GameType === "RS" ? 'team-schedule-item ' + opponent.Conference : 'team-schedule-item ' + props.data.GameType}>
+        <div className={'week'}>
+          {props.data.GameType === "CC" || props.data.GameType === "NC" || props.data.GameType === "RS" ? props.data.WeekNum : props.data.GameType}
+        </div>
+        <div className={'opponent'}>
+          {props.data.AwayTeamData.id === props.teamID ? '@ ' + opponent.TeamName : 'v ' + opponent.TeamName}
+        </div>
+        {props.data.HomeScore && props.data.AwayScore &&
+          <div className={props.data.HomeTeamData.id === props.teamID ? 'score ' + props.data.HomeWL : 'score ' + props.data.AwayWL}>
+            <div className="winloss">{props.data.HomeTeamData.id === props.teamID ? props.data.HomeWL : props.data.AwayWL}</div>
+            <div className="score">{props.data.HomeTeamData.id === props.teamID ? props.data.HomeScore + ' - ' + props.data.AwayScore : props.data.HomeScore + ' - ' + props.data.AwayScore}</div>
+          </div>
+        }
       </div>
-      <div className={'opponent'}>
-        {props.data.AwayTeamData.id === props.teamID ? '@ ' + opponent.TeamName : 'v ' + opponent.TeamName}
-      </div>
-      <div className={props.data.HomeTeamData.id === props.teamID ? 'score ' + props.data.HomeWL : 'score ' + props.data.AwayWL}>
-        <div className="winloss">{props.data.HomeTeamData.id === props.teamID ? props.data.HomeWL : props.data.AwayWL}</div>
-        <div className="score">{props.data.HomeTeamData.id === props.teamID ? props.data.HomeScore + ' - ' + props.data.AwayScore : props.data.HomeScore + ' - ' + props.data.AwayScore}</div>
-      </div>
-    </div>
     </Link>
   )
 }
