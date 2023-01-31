d1f890ff622f1f913ffd1bb0ee363cb62b9e6a8b
dreamsbydutch <71857307+dreamsbydutch@users.noreply.github.com>
2023-01-31 13:29:27 -0500

    Update RankingItem.js

--- src/pages/Home/components/TeamRankings/RankingItem/RankingItem.js
+++ src/pages/Home/components/TeamRankings/RankingItem/RankingItem.js
@@ -7,7 +7,7 @@ function RankingItem(props) {
         <div className="teamName">{props.data.teamData.teamName}</div>
         <div className="teamStats">
             <div className="teamRecord">{props.data.teamData.W+" - "+props.data.teamData.L}</div>
-            <div className="teamConfRecord">{"("+props.data.teamData.confW+" - "+props.data.teamData.confL+")"}</div>
+            <div className="teamConfRecord">{"("+props.data.teamData.CCW+" - "+props.data.teamData.CCL+")"}</div>
             <div className="teamLastWeek">{props.data.teamData.teamID === props.data.lastWeekData.HomeTeam ? props.data.lastWeekData.HomeWL+" "+props.data.lastWeekData.HomeScore+"-"+props.data.lastWeekData.AwayScore+" v "+props.data.lastWeekData.AwayTeamData.TeamName : props.data.lastWeekData.AwayWL+" "+props.data.lastWeekData.AwayScore+"-"+props.data.lastWeekData.HomeScore+" @ "+props.data.lastWeekData.HomeTeamData.TeamName}</div>
         </div>
     </div>
